d3a05d8ae99e0da5c2ce09009f4c3caf4662885c
jguneratne <59374962+jguneratne@users.noreply.github.com>
2023-07-01 07:46:44 -0400

    First attempt at creating elements with src/index.js - styles not applied

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import "./style.css";
 
 console.log("restaurant project");
+
+function components() {
+  const container = document.querySelector("#container");
+  const navBar = document.createElement("div");
+  navBar.className = "nav";
+  const logoDiv = document.createElement("div");
+  logoDiv.className = "logo";
+  const logo = document.createElement("h4");
+
+  container.appendChild(navBar);
+  navBar.appendChild(logoDiv);
+  logoDiv.appendChild(logo);
+  logo.textContent = "Farmstead Kitchen";
+}
+
+document.body.appendChild(components());
